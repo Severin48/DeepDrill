0aee8108dafd9b88168345c92d29096d4563a305
MrRaptorious <35742529+MrRaptorious@users.noreply.github.com>
2024-11-06 16:47:12 +0100

    update metrics calculation to use provided example images

--- test_scripts/calc_metrics.py
+++ test_scripts/calc_metrics.py
@@ -2,32 +2,31 @@
 # https://pypi.org/project/image-similarity-measures/
 # 
 # Usage: 
-# 1. create directory DeepDrill/test_img/
-# 2. load images into the directory with the format img_IMAGENAME_original.jpg where IMAGENAME is the name of the image
-# 3. call this script with the "init" argument (e.g. python test_script/calc_metrics.py init). Downscaled versions of the original images will be created.
-# 4. call this script with metric names as arguments (e.g. python test_script/calc_metrics.py psnr rmse) 
-#
-# You can change the scale_factor in this file to change the overall scaling 
+# 1. call this script with the "init" argument (e.g. python calc_metrics.py init). Downscaled versions of the original images will be created.
+# 4. call this script with metric names as arguments (e.g. python calc_metrics.py psnr rmse) 
 
 import cv2 
 import os
-import glob
 import sys
-from collections import defaultdict
 import image_similarity_measures.evaluate as img_eval
 from prettytable import PrettyTable
-import progressbar
+from tqdm import tqdm
+
+base_path = os.path.join(os.getcwd(),'test_img')
+
+example_files = {
+    # name : [original, original_downscaled]
+   'seahorse1':['original/seahorse1_4k.jpg', 'original/seahorse1_1920x1080.png'],
+   'seahorse2':['original/seahorse2_4k.jpg', 'original/seahorse2_1920x1080.png'],
+   'seahorse3':['original/seahorse3_4k.jpg', 'original/seahorse3_1920x1080.png']
+}
 
 test_img_dir = "test_img"
-scale_factor = 0.125
+scale_factor = 0.5
 name_to_cv2_scaling = {"nn":cv2.INTER_NEAREST,
                        "linear":cv2.INTER_LINEAR,
                        "lanczos4":cv2.INTER_LANCZOS4,
                        "cubic":cv2.INTER_CUBIC}
-                       #"original":cv2.INTER_NEAREST}
-
-def get_files():
-    return glob.glob(os.path.join(test_img_dir, '*_original.jpg'))
 
 def get_img_variants(img, scale_factor=0.5):
     scaled_imgs = {}
@@ -42,74 +41,65 @@ def get_img_variants(img, scale_factor=0.5):
     return scaled_imgs
 
 def create_files():
-    imgs = glob.glob(os.path.join(test_img_dir, '*_original.jpg'))
-
-    for img in imgs:
-        base_name = os.path.basename(img)
-        img_name = base_name.split('_')[1]
-
-        original = cv2.imread(img) 
+    for name, paths in example_files.items():
+        original = cv2.imread(os.path.join(base_path, paths[0])) 
         scaled_imgs = get_img_variants(original,scale_factor=scale_factor)
        
         for k,v in scaled_imgs.items():
-            file_name = f'img_{img_name}_{k}.jpg'
+            file_name = f'{name}_{k}.jpg'
             cv2.imwrite(os.path.join(test_img_dir, file_name), v)
 
-def compare_files(metrics):
-    imgs = glob.glob(os.path.join(test_img_dir, '*.jpg'))
+def calc_metrics(metrics):
+    """
+    calculates the metrics for each image from 'paths' with the image from 'original_path'
+    the images must have all the same size!
+    """
     results = PrettyTable(["Image", "Metric", "Scaling", "Value"])
 
+    with tqdm(total=len(example_files.items())*len(name_to_cv2_scaling.keys())*len(metrics)) as pbar:
+        for name, paths in example_files.items():
+            gt = cv2.imread(os.path.join(base_path, paths[1]))
 
-    # group files
-    groups = defaultdict(list)
-    for img in imgs:
-        base_name = os.path.basename(img)
-        parts = base_name.split('_')
-        image_name = parts[1].split('.')[0]
-        groups[image_name].append(base_name)
+            for metric in metrics:
+                for method in name_to_cv2_scaling.keys():
+                        pred = cv2.imread(os.path.join(base_path, f'{name}_{method}.jpg'))
+                        metric_func = img_eval.metric_functions[metric]
 
-    # compare original with scaled 
-    with progressbar.ProgressBar(max_value=len(imgs) * len(metrics)) as bar:
-        for _,v in groups.items():
-            original_path = os.path.join(test_img_dir, [file for file in v if "original" in file][0])
-            other_paths = [os.path.join(test_img_dir, file) for file in v if "original" not in file]
-            other_paths.sort()
+                        metric_value = float(metric_func(gt, pred))
+                        results.add_row([name, metric,method, round(metric_value,4)])
 
-            original = cv2.imread(original_path) 
-
-            for path in other_paths:
-                base_name = os.path.basename(path)
-                parts = base_name.split('_')
-                interpolation_method = parts[2].split(".")[0]
-
-                scaled = cv2.imread(path) 
+                        pbar.update(1)
                 
-                # ensure same size.... problem because we measure the quality of two scalings...
-                original_height, original_width = original.shape[:2]
-                resized_scaled_image = cv2.resize(scaled, (original_width, original_height), interpolation=name_to_cv2_scaling[interpolation_method])
+            results.add_row(['','','',''])
 
-                for metric in metrics:
-                    metric_func = img_eval.metric_functions[metric]
-                    out_value = float(metric_func(original, resized_scaled_image))
-                    img_eval.logger.info(f"{metric.upper()} value is: {out_value}")
-                    results.add_row([parts[1], metric, parts[2].split(".")[0], round(out_value,4)])
-                    bar.next()
+    return results
+    
+def load_image_with_info(path):
+    base_name = os.path.basename(path)
+    parts = base_name.split('_')
 
-            results.add_row(["-","-","-","-"])
-        
-        print(results)
+    img_name = parts[0]
+    scaling_method = parts[1].split(".")[0]
+    img = cv2.imread(path)
 
+    return (img_name,scaling_method,img)
+  
 def main(args):     
 # IF DEBUG
     if __debug__:
-        #args = ["init"]
-        args = ["ssim","psnr"]
+        # args = ["init"]
+        # rmse, psnr, ssim, fsim, issm
+        args = ["psnr",'ssim']
+        # args = ["hist"]
 # ENDIF DEBUG
 
-    if len(args) > 0 and args[0] == "init":
+    if len(args) == 1 and args[0] == "init":
         create_files()
+    elif len(args) == 1 and args[0] == "hist":
+        pass
     else:
-        compare_files(args)
+        metrics = calc_metrics(metrics=args)
+        print(metrics)
         
 if __name__ == "__main__": 
     main(sys.argv) 
