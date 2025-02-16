7f7c2683126fe4c8b2c443ec0cd2d779d8020be0
MrRaptorious <35742529+MrRaptorious@users.noreply.github.com>
2025-02-16 16:45:16 +0100

    bugfix lanczos filter
    the previous version only sampled between -1 and 1 due to normalization. This version should sample between -A and A. With high A values there are also ringing artefacts which should be the case.

--- shaders/experimental/lanczos2.glsl
+++ shaders/experimental/lanczos2.glsl
@@ -2,9 +2,8 @@
 // Fragmentshader for scaling with Lanczos kernel.
 //
 // Parameter to tweak:
-// A: Size of the Lanczos window and effects the dampening at the edge
-// B: Multiplier to change frequency of sinc
-// With A=2.0 and B=1.5 result looks simelar to bilinear scalilng but sharper
+// A: Size of the Lanczos window, number of lobes of the Lanczos function
+// With A=2.0 result looks simelar to bilinear scalilng but sharper
 // -----------------------------------------------------------------------------
 
 uniform sampler2D curr;
@@ -13,10 +12,8 @@ uniform vec2 size;
 uniform float zoom;
 
 #define PI (3.1415926535897932384626433)
-// Size of Lanczos window damping factor 
-#define A (2.0)
-// Multiplier in sinc for higher frequency
-#define B (1.5)
+// Size of Lanczos window 
+#define A (10.0)
 const int Aint = int(A);
 
 vec2 zoomed(vec2 coord) {
@@ -30,10 +27,8 @@ float sinc(float x) {
 }
 
 float lanczosAt(float x) {
-    if (x == 0.0) return 1.0;
     if (abs(x) >= A) return 0.0;
-    float bX = B * x;
-    return (sinc(bX) * sinc(bX/A));
+    return (sinc(x) * sinc(x/A));
 }
 
 vec4 lanczos(sampler2D sampler, vec2 coord) {
@@ -41,20 +36,17 @@ vec4 lanczos(sampler2D sampler, vec2 coord) {
     float weightSum = 0.0;
 
     vec2 texCoord = coord * size;
-    vec2 texBase = floor(texCoord - 0.5) + 0.5;
-    
+    vec2 texFract = fract(texCoord);
+
     // window of size [-A;A] x [-A;A]
     for (int j = -Aint + 1; j <= Aint; ++j) {
         for (int i = -Aint + 1; i <=Aint; ++i) {
-            vec2 offset = vec2(float(i), float(j));
-            // sample position in pixel space
-            vec2 tap = texBase + offset;
-            vec2 sincDist = (tap - texCoord) / A;
 
-            // 2D lanczos: L(x) * L(y) 
-            float weight = lanczosAt(sincDist.x) * lanczosAt(sincDist.y);
-            
-            color += texture2D(sampler, tap / size) * weight;
+            vec2 offset = vec2(float(i), float(j));
+            vec2 dist = texFract - offset;
+            float weight = lanczosAt(dist.x) * lanczosAt(dist.y);
+
+            color += texture2D(sampler, (floor(texCoord)+offset)/size) * weight;
             weightSum += weight;
         }
     }
@@ -63,23 +55,17 @@ vec4 lanczos(sampler2D sampler, vec2 coord) {
 }
 
 void main() {
-    // Read texel from the current texture
     vec2 coord = gl_TexCoord[0].xy;
     coord.y = 1.0 - coord.y;
     coord = zoomed(coord);
-
     vec4 color1 = lanczos(curr, coord);
 
+    // temporal interpolation 
     if (zoom > 1.0) {
-
-        // Check if a corresponding texel exists in the next texture
         vec2 coord2 = 2.0 * coord - vec2(0.5,0.5);
+
         if (coord2.x >= 0.0 && coord2.x <= 1.0 && coord2.y >= 0.0 && coord2.y <= 1.0) {
-
-            // Read the corresponding texel from the next texture
             vec4 color2 = lanczos(next, coord2);
-
-            // Interpolate between both texels (linear)
             color1 = mix(color1, color2, zoom - 1.0);
         }
     }
